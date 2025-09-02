890b0231f4a2f33e645bd4d9da7742e75d9af040
diogohudson <diogohudson@users.noreply.github.com>
2025-09-02 09:14:59 -0300

    feat: add dates

--- api/wildfire_assessment/views.py
+++ api/wildfire_assessment/views.py
@@ -22,6 +22,22 @@ class EcologicalReserveViewSet(viewsets.ModelViewSet):
     serializer_class = EcologicalReserveSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def calculate_date_range(
+        self, date_str, days_before=60, days_after=60, date_format="%Y-%m-%d"
+    ):
+        if not date_str or not isinstance(date_str, str):
+            return None, None
+        try:
+            date = datetime.strptime(date_str, date_format)
+            date_before = date - timedelta(days=days_before)
+            date_after = date + timedelta(days=days_after)
+            return (
+                (date.strftime(date_format), date_before.strftime(date_format)),
+                (date.strftime(date_format), date_after.strftime(date_format)),
+            )
+        except ValueError:
+            return None, None
+
     @extend_schema(
         methods=["POST"],
         request=None,
@@ -41,29 +57,8 @@ class EcologicalReserveViewSet(viewsets.ModelViewSet):
                 type=OpenApiTypes.DATE,
                 required=True,
             ),
-            OpenApiParameter(
-                name="post_fire_date",
-                description="Post-fire date",
-                type=OpenApiTypes.DATE,
-                required=True,
-            ),
         ],
     )
-
-    def calculate_date_range(self, date_str, days_before=60, days_after=60, date_format="%Y-%m-%d"):
-        if not date_str or not isinstance(date_str, str):
-            return None, None 
-        try:
-            date = datetime.strptime(date_str, date_format)
-            date_before = date - timedelta(days=days_before)
-            date_after = date + timedelta(days=days_after)
-            return (
-                (date.strftime(date_format), date_before.strftime(date_format)),
-                (date.strftime(date_format), date_after.strftime(date_format))
-            )
-        except ValueError:
-            return None, None
-
     @action(detail=True, methods=["post"], url_path="analyze")
     def analyze(self, request, pk=None):
         """
@@ -76,22 +71,14 @@ class EcologicalReserveViewSet(viewsets.ModelViewSet):
         pre_fire_date = request.query_params.get("pre_fire_date")
         post_fire_date = request.query_params.get("post_fire_date")
 
-        pre_fire_date_to, _ = self.calculate_date_range(pre_fire_date)
-        print(pre_fire_date_to)
-        _, post_fire_date_from = self.calculate_date_range(post_fire_date)
-        print(post_fire_date_from)
-        polygon = load_polygon(polygon_path)
-
-        # print(polygon)
-
-        result = WildfireAnalyzer(polygon)
         # You can implement your logic here
         # Example: reserve = self.get_object()
         return Response(
-            {"rgb_pre_fire": f"https://teste.com/image",
-            "polygon_path": polygon_path,
-            "pre_fire_date": pre_fire_date_to,
-            "post_fire_date": post_fire_date_from,
+            {
+                "rgb_pre_fire": f"https://teste.com/image",
+                "polygon_path": polygon_path,
+                "pre_fire_date": pre_fire_date_to,
+                "post_fire_date": post_fire_date_from,
                 "rgb_post_fire": "https://teste.com/image",
                 "ndvi_pre_fire": "https://teste.com/image",
                 "ndvi_post_fire": "https://teste.com/image",
@@ -105,8 +92,8 @@ class EcologicalReserveViewSet(viewsets.ModelViewSet):
                     "low": 17.64,
                     "moderate": 23.03,
                     "high": 12.74,
-                    "very_high": 0.0009
+                    "very_high": 0.0009,
                 },
-                "polygon_area": 8.955
+                "polygon_area": 8.955,
             }
         )
