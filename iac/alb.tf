cc7a9ab2724a3e5c812a20eb9f099d816f6b405b
diogohudson <diogohudson@users.noreply.github.com>
2025-09-16 11:53:52 -0300

    feat: Adds new polygons / add 443 support on ALB

--- iac/alb.tf
+++ iac/alb.tf
@@ -12,7 +12,7 @@ resource "aws_lb" "api" {
   }
 }
 
-# Target group for ECS service
+# Target group for ECS service (remains on port 80 for backend, but ALB will forward 443 to it)
 resource "aws_lb_target_group" "api" {
   name        = "wildfire-assessment-tg-${var.environment}"
   port        = 80
@@ -35,11 +35,29 @@ resource "aws_lb_target_group" "api" {
   }
 }
 
-
+# Listener for ALB (HTTP) - redirect to HTTPS
 resource "aws_lb_listener" "api_http" {
   load_balancer_arn = aws_lb.api.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      protocol    = "HTTPS"
+      port        = "443"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# Listener for ALB (HTTPS)
+resource "aws_lb_listener" "api_https" {
+  load_balancer_arn = aws_lb.api.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-east-1:055213706289:certificate/455b0881-02cf-483a-820d-03852fcc4c90"
 
   default_action {
     type             = "forward"
